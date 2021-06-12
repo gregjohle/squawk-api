1d802469cfe4f6f1e4cf2867f01d8e6bd7d60fe3
Greg Johle <greg.johle@gmail.com>
2021-06-12 12:47:34 -0400

    added socket.io for webRTC routing

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const app = require("./app");
 const { PORT } = require("./config");
 const knex = require("knex");
+const http = require("http");
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
 
 const db = knex({
   client: "pg",
@@ -11,6 +15,32 @@ const db = knex({
 });
 
 app.set("db", db);
+
+const rooms = {};
+
+io.on("connection", (socket) => {
+  socket.on("join room", (roomID) => {
+    if (rooms[roomID]) {
+      rooms[roomID].push(socket.id);
+    } else {
+      rooms[roomID] = [socket.id];
+    }
+    const otherUser = rooms[roomID].find((id) => id !== socket.id);
+    if (otherUser) {
+      socket.emit("other user", otherUser);
+      socket.to(otherUser).emit("user joined", socket.id);
+    }
+  });
+  socket.on("offer", (payload) => {
+    io.to(payload.target).emit("offer", payload);
+  });
+  socket.on("answer", (payload) => {
+    io.to(payload.target).emit("answer", payload);
+  });
+  socket.on("ice-candidate", (incoming) => {
+    io.to(incoming.target).emit("ice-candidate", incoming.candidate);
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
